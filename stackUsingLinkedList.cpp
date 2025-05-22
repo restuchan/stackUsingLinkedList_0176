fcca0e0e0d6521dc596e168aa7422df0773f564b
restuchan <restu.chan188@gmail.com>
2025-05-22 10:33:14 +0700

    cout value nya

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -28,4 +28,8 @@ class Stack {
     newNode->data = value; //2. assign value
     newNode->next = top; //3. set the next pointer of the new node to the current top
     top = newNode; //4. update the top pointer to the new node
-    
+    cout << "Push value = " << value << endl;
+    return value;
+    }
+
+
