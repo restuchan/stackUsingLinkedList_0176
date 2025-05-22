cd842d1511263c67bb33f539f85d42df7f74396d
restuchan <restu.chan188@gmail.com>
2025-05-22 10:40:53 +0700

    menambahkan method void

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -32,4 +32,14 @@ class Stack {
     return value;
     }
 
+//pop operation: remove the topmost element from the stack
+void pop(){
+    if (isEmpty()){
+        cout << "Stack is empty." << endl;
+    }
+    Node* temp = top; //create a temporary pointer to the top node
+    top = top->next; //update the top pointer to the next node
+    cout << "Popped value: " << top->data << endl;
+    }
 
+    
