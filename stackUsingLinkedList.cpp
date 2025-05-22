aa1ce39a69a373a0518926e0549973ecf4dacc4e
restuchan <restu.chan188@gmail.com>
2025-05-22 11:01:35 +0700

    jika memilih 3

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -102,4 +102,15 @@ int main ()
                 }
                 break;
 
+            case 3:
+                if (!stack.isEmpty())
+                {
+                    stack.peek(); //get the value of the top element
+                }
+                else
+                {
+                    cout << "Stack is empty. No top value." << endl;
+                }
+                break;
+
             
