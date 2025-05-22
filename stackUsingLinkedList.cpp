b59334d363297e327b0d103ab0386b84203f9666
restuchan <restu.chan188@gmail.com>
2025-05-22 09:58:56 +0700

    isi kelas Node

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -4,4 +4,11 @@ using namespace std;
 //node class representing a single node in the linked list
 class Node{
     public:
-    
+   int data;
+   Node* next;
+
+    Node(){
+        next = NULL;
+    }
+};
+
