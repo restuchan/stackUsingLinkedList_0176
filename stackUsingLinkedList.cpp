3205491377edc97cb7e49448a5b2ff408726ccd9
restuchan <restu.chan188@gmail.com>
2025-05-22 10:29:49 +0700

    update top pointer ke new Node

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -27,4 +27,5 @@ class Stack {
     Node* newNode = new Node(); //1. allocate memory for the new node
     newNode->data = value; //2. assign value
     newNode->next = top; //3. set the next pointer of the new node to the current top
+    top = newNode; //4. update the top pointer to the new node
     
